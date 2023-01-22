0d0b9e5b31d5dcfd7f134bc0e2b3e33f40b08de2
AntonijoStojanovic <72695652+AntonijoStojanovic@users.noreply.github.com>
2023-01-22 14:14:46 +0100

    Add files via upload

---
+++ NaKvadrat.c
@@ -0,0 +1,28 @@
+﻿/*Napisite program koji ucitava prirodni broj n. Program treba ispisati koliko ima prirodnih brojeva, strogo većih od 1, ciji kvadrat dijeli broj n.
+
+Prenio u Grader: Mateo Drk, RAC 18/19*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+ 
+
+	int broj;
+
+	scanf("%d", &broj);
+
+	int brojac = 0;
+
+	for (int i = 2; i * i <= broj; i++)
+	{
+		if (broj % (i * i) == 0)
+		{
+			brojac++;
+		}
+	}
+
+	printf("%d", brojac);
+
+	return 0;
+
+}
